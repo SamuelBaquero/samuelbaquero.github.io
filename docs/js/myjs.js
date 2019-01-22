a48404daecd5b30c85422298b3539a7c764d8bcd
Samuel Baquero <s.baquero10@uniandes.edu.co>
2019-01-22 11:13:11 -0500

    Late update
    Late update on the rework; still working on it

--- docs/js/myjs.js
+++ docs/js/myjs.js
@@ -1,28 +1,7 @@
 (function($) {
     "use strict";
 
-    var $navbar = $("#about"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
-
-    var $navitems = $(".tflip");
-
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
-
-        if (scrollTop > y_pos + height) {
-            $navbar.addClass("navbar-fixed").animate({
-                top: 0
-            });
-
-            $navitems.addClass("navitem");
-        } else if (scrollTop <= y_pos) {
-            $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-48px"
-            }, 0.1);
-            $navitems.removeClass("navitem").clearQueue();
-        }
-    });
+    
 
 
 })(jQuery, undefined);
